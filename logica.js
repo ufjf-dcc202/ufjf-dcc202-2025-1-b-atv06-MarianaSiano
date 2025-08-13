cc1435c154c92be3699f7866b8e9b528cb7576da
MarianaSiano <marianasianop@gmail.com>
2025-08-12 22:32:50 -0300

    Inicia e/ou reinicia o jogo e tenta selecionar uma peça

--- logica.js
+++ logica.js
@@ -14,4 +14,25 @@ const jogo = {
     ],
     tabuleiro: [],
     pecaSelecionada: null,
+
+    //Inicia ou reinicia o jogo, copiando o estado inicial para o atual
+    iniciar: function() {
+        //Cria uma cópia para não alterar o estado inicial
+        this.tabuleiro = JSON.parse(JSON.stringify(this.tabuleiroInicial));
+        this.pecaSelecionada = null;
+    },
+
+    /**
+     * Tenta selecionar uma peça nas coordenadas dadas.
+     * @param {number} linha - A linha da peça.
+     * @param {number} coluna - A coluna da peça.
+     * @returns {boolean} - True se a seleção foi bem-sucedida, false caso contrário.
+     */
+    selecionarPeca: function(linha, coluna) {
+        if(this.tabuleiro[linha][coluna] === 1) {
+            this.pecaSelecionada = { linha, coluna };
+            return true;
+        }
+        return false;
+    }
 }
